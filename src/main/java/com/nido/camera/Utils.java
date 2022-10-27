4c03865cef5f945af683a2e3637ba57ca04f8837
NidoThe1st <110397805+NidoThe1st@users.noreply.github.com>
2022-10-27 18:58:36 +0200

    Now able to enable/disable cameras

--- src/main/java/com/nido/camera/Utils.java
+++ src/main/java/com/nido/camera/Utils.java
@@ -3,11 +3,19 @@ package com.nido.camera;
 import me.makkuusen.timing.system.track.Track;
 import me.makkuusen.timing.system.track.TrackDatabase;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.nido.camera.newCamCommand.plugin;
 
 public abstract class Utils {
 
@@ -50,10 +58,67 @@ public abstract class Utils {
         String[] xyz = s.split(",");
         return new Vector(Integer.parseInt(xyz[0]), Integer.parseInt(xyz[1]), Integer.parseInt(xyz[2]));
     }
+    //serialization of data
+    public static String disabledToString(ArrayList<Integer> cameraids) {
+        StringBuilder result = new StringBuilder();
+        for (Integer cameraid: cameraids) {
+            result.append(cameraid);
+            result.append(",");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+    //deserialization of data
+    public static ArrayList<Integer> stringToDisabled(String string) {
+        String[] cameraidsString = string.split(",");
+        ArrayList<Integer> cameraids = new ArrayList<>();
+        for (String cameraidString: cameraidsString) {
+            try {Integer cameraid = Integer.parseInt(cameraidString);
+                cameraids.add(cameraid);
+            }
+            catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return cameraids;
+    }
     public static Vector getMin(Location a, Location b) {
         return Vector.getMinimum(a.toVector(), b.toVector());
     }
     public static Vector getMax(Location a, Location b) {
         return Vector.getMaximum(a.toVector(), b.toVector());
+    }
+
+    public static void openMenu(Player player){
+        CamPlayer camPlayer = plugin.getPlayer(player);
+        AtomicInteger counter = new AtomicInteger(10);
+        Inventory inv = Bukkit.createInventory(player,54, ChatColor.AQUA.toString() + ChatColor.BOLD + "Camera Menu");
+        HashMap<Integer, Cam> cameraitems = new HashMap<>();
+        SkullBuilder cameraTemplate = new SkullBuilder(Material.PLAYER_HEAD, 1)
+                .setDisplayName("Camera")
+                .setOwner("e43a2867-f7f1-5dd2-9f3c-6eb405548153");
+        for (Cam camera: plugin.getCameras()) {
+            if(camera.getTrack() != Utils.getClosestTrack(player)) {continue;}
+            if(camera.getLabel() != null) {
+                cameraTemplate.setDisplayName(camera.getLabel());
+            }else {
+                cameraTemplate.setDisplayName(String.valueOf(camera.getIndex()));
+            }
+            if (camPlayer.isCameraDisabled(camera.getId())){
+                cameraTemplate.setLore(ChatColor.RED + "This camera is disabled!");
+            }
+            else {
+                cameraTemplate.setLore("Click to teleport!");
+            }
+            cameraTemplate.setAmmount(camera.getIndex());
+            if(counter.get() + 1 % 9 == 0) {counter.addAndGet(2);}
+            if(counter.get() > 43) {break;}
+            cameraitems.put(counter.get(), camera);
+            inv.setItem(counter.getAndAdd(1), cameraTemplate.build());
+        }
+        camPlayer.setCameraItems(cameraitems);
+        camPlayer.setInv(true);
+        player.openInventory(inv);
     }
 }
--- src/main/java/com/nido/camera/newCamCommand.java
+++ src/main/java/com/nido/camera/newCamCommand.java
@@ -140,31 +140,6 @@ public class newCamCommand extends BaseCommand {
     @CommandPermission("cameras.menu")
     @Subcommand("menu|m")
     public static void onMenu(Player player){
-        CamPlayer camPlayer = plugin.getPlayer(player);
-        AtomicInteger counter = new AtomicInteger(10);
-        Inventory inv = Bukkit.createInventory(player,54, ChatColor.AQUA.toString() + ChatColor.BOLD + "Camera Menu");
-        HashMap<Integer, Cam> cameraitems = new HashMap<>();
-        SkullBuilder cameraTemplate = new SkullBuilder(Material.PLAYER_HEAD, 1)
-                .setDisplayName("Camera")
-                .setLore("Click to teleport!")
-                .setOwner("e43a2867-f7f1-5dd2-9f3c-6eb405548153");
-        for (Cam camera: plugin.getCameras()) {
-            if(camera.getTrack() != Utils.getClosestTrack(player)) {continue;}
-            if(camera.getLabel() != null) {
-                cameraTemplate.setDisplayName(camera.getLabel());
-            }else {
-                cameraTemplate.setDisplayName(String.valueOf(camera.getIndex()));
-            }
-            cameraTemplate.setAmmount(camera.getIndex());
-            if(counter.get() + 1 % 9 == 0) {counter.addAndGet(2);}
-            if(counter.get() > 43) {break;}
-            cameraitems.put(counter.get(), camera);
-            inv.setItem(counter.getAndAdd(1), cameraTemplate.build());
-        }
-        camPlayer.setCameraItems(cameraitems);
-        camPlayer.setInv(true);
-        System.out.println(camPlayer.isInv());
-        player.openInventory(inv);
-
+        Utils.openMenu(player);
     }
 }
